067556406efcfa3aa02177ca53e801bb10fe7649
Krzysztof Kochel <krzysztof.kochel@biol.uni.lodz.pl>
2023-10-16 11:29:37 +0200

    [C4GH-FS] Change access to credentials

--- c4gh-file-service/src/test/groovy/pl/lodz/uni/biobank/foam/c4ghfs/C4GhServiceTest.groovy
+++ c4gh-file-service/src/test/groovy/pl/lodz/uni/biobank/foam/c4ghfs/C4GhServiceTest.groovy
@@ -11,17 +11,17 @@ class C4GhServiceTest extends Specification {
     final SERVER_SEC_KEY = "src/test/resources/server_c4gh.sec"
     final REQUESTER_PUB_KEY = "src/test/resources/requester_c4gh.pub"
     final REQUESTER_SEC_KEY = "src/test/resources/requester_c4gh.sec"
-    final ARCHIVED_FILE = "src/test/resources/encrypted_file"
+    final ARCHIVED_FILE = "encrypted_file"
     final ARCHIVED_FILE_HEADER = "637279707434676801000000010000006c0000000000000078bf6a4ae74f432f0ed6a8a516bf6bd6cdeea5d024245aa6ce7284cd232c2967d1698bb5543c088c606481efcba9494695040e9dc7d326a9d57abd5d1c07b5920426ea81a3ae7560abd2fe845843983173ab943855dc58f2496437cc60af3dfc42cdba34"
-    final RE_ENCRYPTED_FILE = "src/test/resources/requester/file_21.c4gh"
+    final RE_ENCRYPTED_FILE = "src/test/resources/requester/encrypted_file.c4gh"
     final SERVER_PASSPHRASE = 'uxPFPxI4Oe4H5qJ1UO7kMCkvKgNHEzrX'
     final REQUESTER_PASSPHRASE = 'requester'
 
 
     def "Re-encrypt file"() {
         given:
-        def sut = new C4ghService(new PosixArchive(bucketName), new PosixOutbox("src/test/resources/"))
-        def task = new C4ghExportTask(ARCHIVED_FILE_HEADER, ARCHIVED_FILE, "file_21.c4gh", new File(REQUESTER_PUB_KEY).text, "requester")
+        def sut = new C4ghService(new PosixArchive("src/test/resources/"), new PosixOutbox("src/test/resources/"))
+        def task = new C4ghExportTask(ARCHIVED_FILE_HEADER, ARCHIVED_FILE, "encrypted_file.c4gh", new File(REQUESTER_PUB_KEY).text, "requester")
 
         when:
         sut.encryptionWithReceiverPublicKey(task, SERVER_SEC_KEY, SERVER_PASSPHRASE)
