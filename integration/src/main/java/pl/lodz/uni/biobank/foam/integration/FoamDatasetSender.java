410827761bbc1975b9d343c4aa8815e5d03eee13
Krzysztof Kochel <krzysztof.kochel@biol.uni.lodz.pl>
2023-10-25 23:31:55 +0200

    [INT] Add logs for basic path

--- integration/src/main/java/pl/lodz/uni/biobank/foam/integration/FoamDatasetSender.java
+++ integration/src/main/java/pl/lodz/uni/biobank/foam/integration/FoamDatasetSender.java
@@ -1,11 +1,15 @@
 package pl.lodz.uni.biobank.foam.integration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FoamDatasetSender {
+    private static final Logger log = LoggerFactory.getLogger(FoamDatasetSender.class);
+
     @Value("${app.foam-dataset.routing-key}")
     private String routingKey;
     private final RabbitTemplate template;
@@ -16,5 +20,6 @@ public class FoamDatasetSender {
 
     public void handleSend(DatasetData data) {
         template.convertAndSend(routingKey, data);
+        log.info("Dataset with id {} has been send to FOAM with routing key {}", data.stableId(), routingKey);
     }
 }
--- integration/src/main/java/pl/lodz/uni/biobank/foam/integration/SdaListener.java
+++ integration/src/main/java/pl/lodz/uni/biobank/foam/integration/SdaListener.java
@@ -1,10 +1,13 @@
 package pl.lodz.uni.biobank.foam.integration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SdaListener {
+    private static final Logger log = LoggerFactory.getLogger(SdaListener.class);
 
     private final DatasetReleaseService service;
 
@@ -13,7 +16,8 @@ public class SdaListener {
     }
 
     @RabbitListener(queues = "foam_integration")
-    public void receiveMessage(DatasetRelease message) {
-        service.handle(message);
+    public void handleEvent(DatasetRelease event) {
+        log.info("Handle DatasetRelease: {}", event);
+        service.handle(event);
     }
 }
