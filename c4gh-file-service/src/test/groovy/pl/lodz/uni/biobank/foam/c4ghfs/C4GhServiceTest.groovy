7f7a9b909cba0e3bd2837d18de6ce1895c06a346
Krzysztof Kochel <krzysztof.kochel@biol.uni.lodz.pl>
2023-10-31 23:20:44 +0100

    [C4GH-FS] Fix test

--- c4gh-file-service/src/test/groovy/pl/lodz/uni/biobank/foam/c4ghfs/C4GhServiceTest.groovy
+++ c4gh-file-service/src/test/groovy/pl/lodz/uni/biobank/foam/c4ghfs/C4GhServiceTest.groovy
@@ -13,7 +13,7 @@ class C4GhServiceTest extends Specification {
     final REQUESTER_SEC_KEY = "src/test/resources/requester_c4gh.sec"
     final ARCHIVED_FILE = "/john_doe/encrypted_file"
     final ARCHIVED_FILE_HEADER = "637279707434676801000000010000006c0000000000000078bf6a4ae74f432f0ed6a8a516bf6bd6cdeea5d024245aa6ce7284cd232c2967d1698bb5543c088c606481efcba9494695040e9dc7d326a9d57abd5d1c07b5920426ea81a3ae7560abd2fe845843983173ab943855dc58f2496437cc60af3dfc42cdba34"
-    final RE_ENCRYPTED_FILE = "src/test/resources/requester/encrypted_file.c4gh"
+    final RE_ENCRYPTED_FILE = "src/test/resources/requester/EGAD44444444/encrypted_file.c4gh"
     final SERVER_PASSPHRASE = 'uxPFPxI4Oe4H5qJ1UO7kMCkvKgNHEzrX'
     final REQUESTER_PASSPHRASE = 'requester'
     def stageSender = Mock(ExportStageSender)
@@ -22,7 +22,7 @@ class C4GhServiceTest extends Specification {
     def "Re-encrypt file"() {
         given:
         def sut = new C4ghService(new PosixArchiveFileTransmitter("src/test/resources/"), new PosixOutboxFileTransmitter("src/test/resources/", stageSender), stageSender)
-        def task = new C4ghExportTask(UUID.randomUUID(), ARCHIVED_FILE_HEADER, ARCHIVED_FILE, "ted_owl/encrypted_file.c4gh", new File(REQUESTER_PUB_KEY).text, "requester", "EGAF00000001")
+        def task = new C4ghExportTask(UUID.randomUUID(), ARCHIVED_FILE_HEADER, ARCHIVED_FILE, "ted_owl/encrypted_file.c4gh", new File(REQUESTER_PUB_KEY).text, "requester", "EGAF00000001", "EGAD44444444")
 
         when:
         sut.encryptionWithReceiverPublicKey(task, SERVER_SEC_KEY, SERVER_PASSPHRASE)
