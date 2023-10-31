af8f2f151bfa21f4c563227086b75ee11776154f
Krzysztof Kochel <krzysztof.kochel@biol.uni.lodz.pl>
2023-10-31 11:43:22 +0100

    [WEB] Added c4gh key availability verification

--- download-manager-web/src/header/AuthenticatedHeader.tsx
+++ download-manager-web/src/header/AuthenticatedHeader.tsx
@@ -1,4 +1,4 @@
-import {Button, Navbar} from "react-bootstrap";
+import {Alert, Button, Navbar} from "react-bootstrap";
 import fegaLogo from '../assets/FEGA-logo-generic.svg'
 import {FC, useContext, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
@@ -14,7 +14,7 @@ interface HeaderResponse {
 
 export const AuthenticatedHeader: FC = () => {
     const {handleSignOut, setToken} = useContext(AuthContext)
-    const [fullName, setFullName] = useState<string>()
+    const [userState, setUserState] = useState<HeaderResponse>()
 
     const href = "/api/cega-users/info";
     const navigate = useNavigate()
@@ -27,32 +27,40 @@ export const AuthenticatedHeader: FC = () => {
 
     useEffect(() => {
         if (data) {
-            setFullName(data.fullName)
+            setUserState(data)
         }
     }, [data]);
 
     return (
-        <div id={"header"}>
-            <Navbar>
-                <Navbar.Brand href="https://ega-archive.org/federated">
-                    <img src={fegaLogo}
-                         width={"190px"}
-                         height={"75px"}
-                         className="d-inline-block align-top" alt="FEGA logo"/>
-                </Navbar.Brand>
-                <img src={genomicMapOfPoland}
-                     width={"190px"}
-                     height={"75px"}
-                     className="d-inline-block align-top" alt="Genomic Map of Poland logo"/>
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text className={"fw-bold"}>
-                        Signed in as: {fullName}
-                    </Navbar.Text>
-                    <Button variant={"outline-primary"} className={"ms-4"}
-                            onClick={() => handleSignOut(setToken, navigate)}>Sign out</Button>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
+        <>
+            {userState ?
+                <div id={"header"}>
+                    <Navbar>
+                        <Navbar.Brand href="https://ega-archive.org/federated">
+                            <img src={fegaLogo}
+                                 width={"190px"}
+                                 height={"75px"}
+                                 className="d-inline-block align-top" alt="FEGA logo"/>
+                        </Navbar.Brand>
+                        <img src={genomicMapOfPoland}
+                             width={"190px"}
+                             height={"75px"}
+                             className="d-inline-block align-top" alt="Genomic Map of Poland logo"/>
+                        <Navbar.Collapse className="justify-content-end">
+                            <Navbar.Text className={"fw-bold"}>
+                                Signed in as: {userState.fullName}
+                            </Navbar.Text>
+                            <Button variant={"outline-primary"} className={"ms-4"}
+                                    onClick={() => handleSignOut(setToken, navigate)}>Sign out</Button>
+                        </Navbar.Collapse>
+                    </Navbar>
+
+                    {!userState.c4ghKeyPresent ?
+                        <Alert variant={"danger"}> Please upload the C4GH public key in ega-archive.org Without the key,
+                            exporting files is impossible.</Alert>
+                        : null}
+                </div> : null}
+        </>
     )
 
 }
