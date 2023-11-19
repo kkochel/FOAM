2b0d820280aa5d56a77ff4076ac1433594482f77
Krzysztof Kochel <krzysztof.kochel@biol.uni.lodz.pl>
2023-11-19 21:15:05 +0100

    [APP] Fix sign out

--- download-manager-app/src/main/java/pl/lodz/uni/biobank/foam/app/SecurityConfiguration.java
+++ download-manager-app/src/main/java/pl/lodz/uni/biobank/foam/app/SecurityConfiguration.java
@@ -3,16 +3,12 @@ package pl.lodz.uni.biobank.foam.app;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -31,16 +27,14 @@ public class SecurityConfiguration extends GlobalAuthenticationConfigurerAdapter
 
     private final JwtFilter jwtFilter;
     private final AuthenticationProvider authenticationProvider;
-    private final LogoutHandler logoutHandler;
     private final MyBasicAuthenticationEntryPoint entryPoint;
 
     @Value("${application.allowed-origins}")
     private String[] allowedOrigins;
 
-    public SecurityConfiguration(JwtFilter jwtFilter, AuthenticationProvider authenticationProvider, LogoutHandler logoutHandler, MyBasicAuthenticationEntryPoint entryPoint) {
+    public SecurityConfiguration(JwtFilter jwtFilter, AuthenticationProvider authenticationProvider, MyBasicAuthenticationEntryPoint entryPoint) {
         this.jwtFilter = jwtFilter;
         this.authenticationProvider = authenticationProvider;
-        this.logoutHandler = logoutHandler;
         this.entryPoint = entryPoint;
     }
 
@@ -58,9 +52,7 @@ public class SecurityConfiguration extends GlobalAuthenticationConfigurerAdapter
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
                 .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .exceptionHandling(h-> h.authenticationEntryPoint(entryPoint))
-                .logout((logout) -> logout.logoutUrl("api/auth/sign-out")
-                        .addLogoutHandler(logoutHandler)
-                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT)));
+                .logout((logout) -> logout.logoutUrl("api/auth/sign-out"));
 
         return http.build();
     }
--- download-manager-app/src/main/java/pl/lodz/uni/biobank/foam/app/authentication/AuthController.java
+++ download-manager-app/src/main/java/pl/lodz/uni/biobank/foam/app/authentication/AuthController.java
@@ -1,17 +1,18 @@
 package pl.lodz.uni.biobank.foam.app.authentication;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
     private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+    private static final String CLEAR_SITE_DATA_HEADER = "Clear-Site-Data";
 
     private final AuthService service;
 
@@ -40,4 +41,10 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @PostMapping("sign-out")
+    public void sigOut(HttpServletResponse response) {
+        response.addHeader(CLEAR_SITE_DATA_HEADER, "\"cache\", \"cookies\"");
+        response.setStatus(HttpStatus.NO_CONTENT.value());
+        SecurityContextHolder.clearContext();
+    }
 }
