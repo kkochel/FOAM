e69b3406733c189607c1d3c37a718061b7562a75
Krzysztof Kochel <krzysztof.kochel@biol.uni.lodz.pl>
2024-12-02 21:33:15 +0100

    [OUTBOX][C4GH-FS][INT] Add probes

--- outbox/src/main/java/pl/lodz/uni/biobank/foam/outbox/OutboxConfig.java
+++ outbox/src/main/java/pl/lodz/uni/biobank/foam/outbox/OutboxConfig.java
@@ -4,15 +4,12 @@ package pl.lodz.uni.biobank.foam.outbox;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity
 public class OutboxConfig {
-    private static final String[] WHITE_LIST_URL = {"/actuator/health/liveness", "/actuator/health/readiness"};
 
     @Bean
     public RestTemplate restTemplate() {
@@ -24,15 +21,7 @@ public class OutboxConfig {
         return new Jackson2JsonMessageConverter();
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(a ->
-                a.requestMatchers(WHITE_LIST_URL)
-                        .permitAll()
-                        .anyRequest().authenticated());
 
-        return http.build();
-    }
 
 
 }
